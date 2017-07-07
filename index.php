582da720046c63c04fe6eafad651fee6574a133e
ishika26 <ishika.banerjee26@gmail.com>
2017-07-07 14:33:09 +0530

    refactoring

--- index.php
+++ index.php
@@ -8,16 +8,17 @@
 </head>
 <body>
 
-	
+<div>
   <div class="camera">
     <video id="video">Video stream not available.</video>
     <button id="startbutton">Take photo</button> 
   </div>
 
-  
+  <canvas id="canvas"></canvas>
   <div class="output">
   <img id="photo" alt="The screen capture will appear in this box."> 
   </div>
+ </div>
 
 </body>
 </html>
