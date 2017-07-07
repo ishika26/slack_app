611b36b990bf531d96efe519b11273dcf0005421
ishika <ibanerjee@xebia.com>
2017-07-07 11:21:14 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	
+<video id="video"></video>
+<canvas id="canvas" style="display:none;"></canvas>
+<div id="buttoncontent"></div>
+<button id="startbutton">CAPTURE</button>
+
+<script type="capture.js"></script>
+</head>
+
+</html>
